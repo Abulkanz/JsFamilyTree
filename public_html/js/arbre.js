a697ac11331dec089852eb72a8523204f41c99ab
Abulkanz <alexandre.violeau@outlook.com>
2017-02-12 18:52:15 +0100

    Apparence et Identités

--- public_html/js/arbre.js
+++ public_html/js/arbre.js
@@ -1,6 +1,10 @@
 "use strict";
-var symHom =  "img/logoHomme.png";
+
+var symGp =  "img/graPerAv.png";
+var symGm = "img/graMerAv.png"
+
 var symFem = "img/logoFemme.png";
+
 var incM = "inconnu";
 var incF = "inconnue";
 var incMp = "inconnus";
@@ -12,26 +16,24 @@ var fem = "Feminin";
 
 
 //Person(name, firstname, age, kind, profession, mother, father, brothers, sisters, profilPicture)
-var graPerPat = new Person(nomP, "Michel", 67, masc, "Marin", incM, incF, "inconnus", "inconnues", symHom);
-var graMerPat = new Person(nomP, 75, "Thérèse", fem, "Femme au foyer", incM, incF, "inconnus", "inconnues", symFem);
-var graPerMat = new Person("Stern", "Gabriel", 61, masc, "inconnu", incM, incF, "inconnus", "inconnues", symHom);
-var graMerMat = new Person("Stern", "Cordier", 68, fem, "Couturière", incM, incF, "inconnus", "inconnues", symFem);
-var pere = new Person(nomP, "Baptiste", masc, "Ouvrier", graMerPat.firstname + graMerPat.name, graPerPat.firstname + graPerPat.name, "inconnus", "inconnues", symHom);
-var mere = new Person("Duval", "Cordier", fem, "Couturière", incM, incF, "inconnus", "inconnues", symFem);
-var ainee = new Person(nomP, "René", masc, "Agriculteur", incM, incF, "inconnus", "inconnues", symHom);
-var gendre = new Person("Tokeida", "Zatoichi", masc, "Cuisinier", incM, incF, incMp, incFp, symHom);
-var puinee = new Person(nomP, "Claudia", 24, fem, "Institutrice", pere.firstName + " " + pere.name, mere.firstName + " " + mere.name, incMp, incFp, symFem);
-var aineeAinee = new Person(nomP, "Cordier", fem, "Couturière", incM, incF, "inconnus", "inconnues", symFem);
-var puineeAinee = new Person("Paulette", "Cordier", fem, "Couturière", incM, incF, "inconnus", "inconnues", symFem);
-var benjAinee = new Person("Paulette", "Cordier", fem, "Couturière", incM, incF, "inconnus", "inconnues", symFem);
-var soeurAine = new Person(nomP, "Cordier", fem, "Couturière", incM, incF, "inconnus", "inconnues", symFem);
-var broInLaw = new Person("Bruce", "Lee", masc, "Karateka", "Chuck Norris", "Rosa Parks", "inconnus", "inconunes", symHom);
-var frerNeveuUn = new Person("LeFrapper", "René", masc, "Agriculteur", incM, incF, "inconnus", "inconnues", symHom);
-var frerNeveuDeux = new Person("LeFrapper", "René", masc, "Agriculteur", incM, incF, "inconnus", "inconnues", symHom);
+var graPerPat = new Person(nomP, "Michel", 67, masc, "Marin", incM, incF, defaut, defaut, symGp);
+var graMerPat = new Person(nomP, "Thérèse", 65, fem, "Femme au foyer", incM, incF, defaut, defaut, symGm);
+var graPerMat = new Person("Stern", "Gabriel", 61, masc, "inconnu", incM, incF, defaut, defaut, symGp);
+var graMerMat = new Person("Stern", "Mireille", 68, fem, "Couturière", incM, incF, defaut, defaut, symGm);
+var pere = new Person(nomP, "Baptiste", 45, masc, "Ouvrier", graPerPat, graMerPat, defaut, defaut, symHom);
+var mere = new Person("Duval", "Suzette", 44, fem, "Couturière", graPerMat, graMerMat, defaut, defaut, symFem);
+var ainee = new Person(nomP, "Adèle", masc, 27, "Agriculteur", pere, mere, defaut, defaut, symHom);
+var gendreA = new Person("Tokeida", "Zatoichi", 32, masc, "Cuisinier", incM, incF, defaut, defaut, symHom);
+var puinee = new Person(nomP, "Claudia", 24, fem, "Institutrice", pere, mere, defaut, defaut, symFem);
+var gendreP = new Person("Pommereau", "Paul", 39, masc, "Comedien", incM, incF, defaut, defaut, symHom);
+var cadet = new Person(nomP, "Maurice", 22, masc, "Cuisinier", pere, mere, defaut, defaut, symHom);
+var bru = new Person("AlBladiya", "Sonia", 22, fem, "Cuisinier", incM, incF, defaut, defaut, symHom);
+var benj = new Person(nomP, "Bruno", masc, 19, "Cuisinier", pere, mere, defaut, defaut, symHom);
+var aineeAinee = new Person("Tokeida", "Kimiyo", fem, 8, defaut, gendreA, ainee, defaut, defaut, symFem);
+var puineeAinee = new Person("Tokeida", "Natsu", fem, 6, defaut, gendreA, ainee, defaut, defaut, symFem);
+var benjAinee = new Person("Tokeida", "Takao", fem, 2, defaut, gendreA, ainee, defaut, defaut, symFem);
+var aineePuinee = new Person("Pommereau", "Hélène", fem, 2, defaut, incM, incF, defaut, defaut, symHom);
 
-alert(graPerPat.kind);
-console.log(graPerPat);
-        
 document.getElementById("symGp").src = symHom;
 document.getElementById("grPp").innerHTML = graPerPat.firstname + " " + graPerPat.name;
 document.getElementById("ageGrPp").innerHTML = graPerPat.age;
--- public_html/js/person.js
+++ public_html/js/person.js
@@ -1,36 +1,38 @@
 "use strict";
 
-function Person(name, firstname, age, kind, profession, father, mother, brothers, sisters, profilPicture){
-  this.name = name;
-  this.firstname = firstname;
-  this.age = age;
-  this.kind = kind;
-  this.profession = profession;
-  this.mother = mother;
-  this.father = father;
-  this.brothers = brothers;
-  this.sisters = sisters;
-  this.profilPicture = profilPicture;
-};
+function Person(name, firstname, age, kind, profession, father, mother, brothers, sisters, profilPicture) {
+    this.name = name;
+    this.firstname = firstname;
+    this.age = age;
+    this.kind = kind;
+    this.profession = profession;
+    this.mother = mother;
+    this.father = father;
+    this.brothers = brothers;
+    this.sisters = sisters;
+    this.profilPicture = profilPicture;
+}
+;
 
-/*
-function addBrotherOrSister(){
-  var genre = prompt("H pour homme, F pour femme");
-    if (genre == "H" || genre == "h"){
-      var frere = new Person;
-      frere.name = prompt("Nom de famille");
-        //Boucle s'il n'y a pas de prénom
-      do {frere.firstName = prompt("Prénom");
-      }while (frere.firstName == (reference == null || reference == undefined || reference == ""));
-        //Boucle si ce n'est pas un chiffre
-      do{frere.age = prompt("Age");
-      }while (isNaN(frere.age));
-      frere.kind = masc;
-      frere.profession = prompt("Profession");
-      frere.mother = mere;
-      frere.father = pere;
-      frere.brothers = bros[];
-      frere.sisters = sistas[];
-    }else if (genre == "F" || genre == "f"){
-    }
-}*/
+
+//function addBrotherOrSister() {
+//    var genre = prompt("H pour homme, F pour femme");
+//    if (genre == "H" || genre == "h") {
+//        var frere = new Person
+//        (
+//                //Boucle s'il n'y a pas de prénom
+//                do {
+//                    frere.firstName = prompt("Prénom");
+//                } while (frere.firstName == (reference == null || reference == undefined || reference == ""));
+//        //Boucle si ce n'est pas un chiffre
+//        do {
+//            frere.age = prompt("Age");
+//        } while (isNaN(frere.age));
+//        frere.kind = masc;
+//        frere.profession = prompt("Profession");
+//        frere.mother = mere;
+//        frere.father = pere;
+//        );
+//    } else if (genre == "F" || genre == "f") {
+//    }
+//}
