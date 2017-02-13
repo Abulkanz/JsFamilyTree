a677e3acf0493c415ad4c5fd7f2e0f2bd18994dd
Abulkanz <alexandre.violeau@outlook.com>
2017-02-13 22:29:04 +0100

    Anglicisationnisation

--- public_html/js/arbre.js
+++ public_html/js/arbre.js
@@ -2,7 +2,6 @@
 
 var symGp =  "img/graPerAv.png";
 var symGm = "img/graMerAv.png"
-
 var symFem = "img/logoFemme.png";
 
 var incM = "inconnu";
@@ -16,23 +15,25 @@ var fem = "Feminin";
 
 
 //Person(name, firstname, age, kind, profession, mother, father, brothers, sisters, profilPicture)
-var graPerPat = new Person(nomP, "Michel", 67, masc, "Marin", incM, incF, defaut, defaut, symGp);
-var graMerPat = new Person(nomP, "Thérèse", 65, fem, "Femme au foyer", incM, incF, defaut, defaut, symGm);
-var graPerMat = new Person("Stern", "Gabriel", 61, masc, "inconnu", incM, incF, defaut, defaut, symGp);
-var graMerMat = new Person("Stern", "Mireille", 68, fem, "Couturière", incM, incF, defaut, defaut, symGm);
-var pere = new Person(nomP, "Baptiste", 45, masc, "Ouvrier", graPerPat, graMerPat, defaut, defaut, symHom);
-var mere = new Person("Duval", "Suzette", 44, fem, "Couturière", graPerMat, graMerMat, defaut, defaut, symFem);
-var ainee = new Person(nomP, "Adèle", masc, 27, "Agriculteur", pere, mere, defaut, defaut, symHom);
-var gendreA = new Person("Tokeida", "Zatoichi", 32, masc, "Cuisinier", incM, incF, defaut, defaut, symHom);
-var puinee = new Person(nomP, "Claudia", 24, fem, "Institutrice", pere, mere, defaut, defaut, symFem);
-var gendreP = new Person("Pommereau", "Paul", 39, masc, "Comedien", incM, incF, defaut, defaut, symHom);
-var cadet = new Person(nomP, "Maurice", 22, masc, "Cuisinier", pere, mere, defaut, defaut, symHom);
-var bru = new Person("AlBladiya", "Sonia", 22, fem, "Cuisinier", incM, incF, defaut, defaut, symHom);
-var benj = new Person(nomP, "Bruno", masc, 19, "Cuisinier", pere, mere, defaut, defaut, symHom);
-var aineeAinee = new Person("Tokeida", "Kimiyo", fem, 8, defaut, gendreA, ainee, defaut, defaut, symFem);
-var puineeAinee = new Person("Tokeida", "Natsu", fem, 6, defaut, gendreA, ainee, defaut, defaut, symFem);
-var benjAinee = new Person("Tokeida", "Takao", fem, 2, defaut, gendreA, ainee, defaut, defaut, symFem);
-var aineePuinee = new Person("Pommereau", "Hélène", fem, 2, defaut, incM, incF, defaut, defaut, symHom);
+
+
+var patGranFa = new Person(nomP, "Michel", 67, masc, "Marin", incM, incF, defaut, defaut, "img/granFaAv.png");
+var patGranMo = new Person(nomP, "Thérèse", 65, fem, "Femme au foyer", incM, incF, defaut, defaut, "img/granMoAv.png");
+var matGranFa = new Person("Stern", "Gabriel", 61, masc, "inconnu", incM, incF, defaut, defaut, "img/granFaAv.png");
+var matGranMo = new Person("Stern", "Mireille", 68, fem, "Couturière", incM, incF, defaut, defaut, "img/granMoAv.png");
+var father = new Person(nomP, "Baptiste", 45, masc, "Ouvrier", patGranFa, patGranMo, defaut, defaut, "img/fathAv");
+var mother = new Person("Duval", "Suzette", 44, fem, "Couturière", matGranFa, matGranMo, defaut, defaut, "img/mothAv.png");
+var firstBorn = new Person(nomP, "Adèle", masc, 27, "Agriculteur", father, mother, array(youngerM, junior), array(youngerF), symHom);
+var sonInLaw1 = new Person("Tokeida", "Zatoichi", 32, masc, "Cuisinier", incM, incF, defaut, defaut, symHom);
+var youngerF = new Person(nomP, "Claudia", 24, fem, "Institutrice", father, mother, array(youngerM, junior), array(firstBorn), symFem);
+var sonInLaw2 = new Person("Pommereau", "Paul", 39, masc, "Comedien", incM, incF, defaut, defaut, symHom);
+var youngerM = new Person(nomP, "Maurice", 22, masc, "Cuisinier", father, mother, array(junior), array(firstBorn, youngerF), symHom);
+var daughInLaw = new Person("AlBladiya", "Sonia", 22, fem, "Cuisinier", incM, incF, defaut, defaut, symHom);
+var junior = new Person(nomP, "Bruno", masc, 19, "Cuisinier", father, mother, array(youngerM), array(firstBorn, youngerF), symHom);
+var fBfirstBorn = new Person("Tokeida", "Kimiyo", fem, 8, defaut, sonInLaw1, firstBorn, defaut, defaut, symFem);
+var fByounger = new Person("Tokeida", "Natsu", fem, 6, defaut, sonInLaw1, firstBorn, defaut, defaut, symFem);
+var fBjunior = new Person("Tokeida", "Takao", fem, 2, defaut, sonInLaw1, firstBorn, defaut, defaut, symFem);
+var yFfirstBorn = new Person("Pommereau", "Hélène", fem, 2, defaut, sonInLaw2, youngerF, defaut, defaut, symHom);
 
 document.getElementById("symGp").src = symHom;
 document.getElementById("grPp").innerHTML = graPerPat.firstname + " " + graPerPat.name;
